9bdce76c5786183a20eac901b24df7aeb56dbca6
hellolzc <lzc80234@qq.com>
2017-06-29 10:20:31 +0800

    half work

--- tools/generateListHelp.sh
+++ tools/generateListHelp.sh
@@ -1,10 +1,16 @@
 python generateFileList.py
-shuf config.list | shuf > config/all.txt
+shuf config.list  > config/all.txt
 
+cd config
 head -130 all.txt > train.list
 head -160 all.txt | tail -30 > val.list 
 head -190 all.txt | tail -30 > test.list 
 
+cd ..
+rm ./tfrecords/train/*
+rm ./tfrecords/val/*
+rm ./tfrecords/test/*
+python convert_to_records.py
 find ./tfrecords/train/ -name '*.tfrecords' | shuf > ./tfrecords/train_tf.list
 find ./tfrecords/val/ -name '*.tfrecords' | shuf > ./tfrecords/val_tf.list
 find ./tfrecords/test/ -name '*.tfrecords' | shuf > ./tfrecords/test_tf.list
